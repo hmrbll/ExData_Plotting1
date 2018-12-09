c16a7dc039e6eb67abc9f16799c9c89d50757544
hmrbll <huseyinemre_balli@hotmail.com>
2018-12-09 17:32:38 +0300

    Break

---
+++ plot1.R
@@ -0,0 +1,12 @@
+file_path <- "household_power_consumption.txt"
+
+data <- read.table(file_path, stringsAsFactors = FALSE, sep = ";", header = TRUE)
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+selector <- data$Date == "2007-02-01" | data$Date == "2007-02-02"
+subdata <- data[selector, ]
+
+subdata$Global_active_power <- as.numeric(subdata$Global_active_power)
+
+png("plot1.png", width = 480, height = 480)
+hist(subdata$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")

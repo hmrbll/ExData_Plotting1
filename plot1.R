dd38a9aac4f5d2ded95cad7caa91ded2902d1504
hmrbll <huseyinemre_balli@hotmail.com>
2018-12-09 17:46:21 +0300

    Plot 1

--- plot1.R
+++ plot1.R
@@ -10,3 +10,4 @@ subdata$Global_active_power <- as.numeric(subdata$Global_active_power)
 
 png("plot1.png", width = 480, height = 480)
 hist(subdata$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()

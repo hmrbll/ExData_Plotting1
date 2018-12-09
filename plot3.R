0a2b5103491204292261e50d4dfbee7e0780f805
hmrbll <huseyinemre_balli@hotmail.com>
2018-12-09 19:40:47 +0300

    Plot 3

---
+++ plot3.R
@@ -0,0 +1,24 @@
+file_path <- "household_power_consumption.txt"
+
+data <- read.table(file_path, stringsAsFactors = FALSE, sep = ";", header = TRUE)
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+selector <- data$Date == "2007-02-01" | data$Date == "2007-02-02"
+subdata <- data[selector, ]
+
+
+Date_time <- paste(subdata$Date, subdata$Time)
+subdata <- subdata[ ,c(-1,-2)]
+subdata <- cbind(Date_time, subdata, stringsAsFactors = FALSE)
+subdata$Date_time <- strptime(subdata$Date_time, format = "%Y-%m-%d %H:%M:%S")
+subdata$Sub_metering_1 <- as.numeric(subdata$Sub_metering_1)
+subdata$Sub_metering_2 <- as.numeric(subdata$Sub_metering_2)
+
+png("plot3.png", width = 480, height = 480)
+
+plot(subdata$Date_time, subdata$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(subdata$Date_time, subdata$Sub_metering_2, col = "red")
+lines(subdata$Date_time, subdata$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1, 1, 1))
+
+dev.off()
